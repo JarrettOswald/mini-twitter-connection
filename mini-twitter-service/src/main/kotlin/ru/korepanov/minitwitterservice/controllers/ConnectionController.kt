b559098b859caa5f8628448b5c58d92befafcac2
JarrettOswald <6jk4LuCtT>
2023-04-09 17:17:35 +0400

    add fix crud

--- mini-twitter-service/src/main/kotlin/ru/korepanov/minitwitterservice/controllers/ConnectionController.kt
+++ mini-twitter-service/src/main/kotlin/ru/korepanov/minitwitterservice/controllers/ConnectionController.kt
@@ -2,21 +2,31 @@ package ru.korepanov.minitwitterservice.controllers
 
 
 import org.openapitools.api.ConnectionsApi
-import org.openapitools.model.Connection
+import org.openapitools.model.ConnectionRead
+import org.openapitools.model.ConnectionWrite
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import ru.korepanov.minitwitterservice.repositories.UsersRepository
+import ru.korepanov.minitwitterservice.repositories.ConnectionsRepository
+import ru.korepanov.minitwitterservice.tables.pojos.Connections
+import java.util.*
 
 @Controller
-class ConnectionController(val usersRepository: UsersRepository) : ConnectionsApi {
+class ConnectionController(val connectionsRepository: ConnectionsRepository) : ConnectionsApi {
 
-    override fun getConnectionByUserName(userId: Long): ResponseEntity<List<Connection>> {
-        println("boooo")
+    override fun getConnectionByUserName(userId: UUID): ResponseEntity<List<ConnectionRead>> {
+        connectionsRepository.fetchById(userId)
         return super.getConnectionByUserName(userId)
     }
 
-    override fun getConnections(): ResponseEntity<List<Connection>> {
-        println("fooo")
-        return super.getConnections()
+    override fun getConnections(): ResponseEntity<List<ConnectionRead>> {
+        val list: List<ConnectionRead> = connectionsRepository.findAll()
+            .map { ConnectionRead(it.id, it.follower, it.followed) }
+        return ResponseEntity(list, HttpStatusCode.valueOf(200))
+    }
+
+    override fun saveConnection(connectionWrite: ConnectionWrite): ResponseEntity<String> {
+        connectionsRepository.insert(Connections(UUID.randomUUID(), connectionWrite.follower, connectionWrite.followed))
+        return ResponseEntity("connection added", HttpStatusCode.valueOf(200))
     }
 }
--- mini-twitter-service/src/main/kotlin/ru/korepanov/minitwitterservice/repositories/ConnectionsRepository.kt
+++ mini-twitter-service/src/main/kotlin/ru/korepanov/minitwitterservice/repositories/ConnectionsRepository.kt
@@ -4,8 +4,11 @@ package ru.korepanov.minitwitterservice.repositories
 import org.jooq.Configuration
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
+import ru.korepanov.minitwitterservice.Tables
 import ru.korepanov.minitwitterservice.tables.daos.ConnectionsDao
 import ru.korepanov.minitwitterservice.tables.daos.UsersDao
+import ru.korepanov.minitwitterservice.tables.pojos.Connections
+import ru.korepanov.minitwitterservice.tables.records.ConnectionsRecord
 
 @Repository
 class ConnectionsRepository(private val dslContext: DSLContext, configuration: Configuration) : ConnectionsDao(configuration) {
